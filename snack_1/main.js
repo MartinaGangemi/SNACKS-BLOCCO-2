8b00beb326d90ac8047e63ebda66b9ff48e0e88b
MartinaGangemi <martinagangemi94@gmail.com>
2022-03-31 12:48:50 +0200

    snack_1

---
+++ snack_1/main.js
@@ -0,0 +1,20 @@
+// ciclo for
+
+// let somma=0
+
+// for (i=1; i<=5; i++){
+// let userNumber = parseInt(prompt("Inserisci un numero"))
+// somma = somma + userNumber
+// }
+// console.log(somma)
+
+// ciclo while
+let somma = 0
+index=1
+
+while (index <= 5){
+    let userNumber = parseInt(prompt("Inserisci un numero"))
+    somma = somma + userNumber
+    index++
+}
+console.log(somma)
