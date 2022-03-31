cd783d3df19afee9de38707f17d2a61867ae00b2
MartinaGangemi <martinagangemi94@gmail.com>
2022-03-31 14:43:38 +0200

    snack_2

--- snack_2/main.js
+++ snack_2/main.js
@@ -1,26 +1,6 @@
 // Inserisci un numero, se è pari stampa il numero,
 // se è dispari stampa il numero successivo.
 
-
-
-
-// creo un ciclo dove chiedo all'utente di inserire un numero, 
-// continuo a chiederlo finchè non mi inserisce un numero pari
-
-// let userNumber 
-// index=0
-
-// while (index = userNumber %2==0 ){
-//     let userNumber = parseInt(prompt("Inserisci un numero"))
-//     if (userNumber %2 !=0 ){
-//         alert("inserisci un numero pari!")
-//     } else{
-//         console.log(`${userNumber} è un numero pari`)
-//     }
-    
-//     index++
-// }
-
 let userNumber = parseInt(prompt("Inserisci un numero"))
 if (userNumber % 2 != 0){
     console.log(`il numero pari successivo di ${userNumber} è ${userNumber+1}`)
